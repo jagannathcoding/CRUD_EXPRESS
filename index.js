b31f13948c6e7c2684aa41dc02b2fd6f21848a20
jagannathcoding <jagannathdebgunin@gmail.com>
2024-11-04 23:40:47 +0530

    For get and post

---
+++ index.js
@@ -0,0 +1,53 @@
+const express=require("express");
+
+const app=express();
+
+
+////SINGLE USER
+
+const user=[{
+    name:"John",
+    kidneys:[{
+        healthy:false
+    }]
+}];
+
+app.use(express.json());
+
+//////FOR GET
+app.get("/",function(req,res){
+    const johnKidneys=user[0].kidneys;
+    const numberOfKidneys=johnKidneys.length;
+    let numberOfHealthyKidneys=0;
+    for(let i=0;i<numberOfKidneys.length;i++)
+    {
+        if(johnKidneys[i].healthy)
+        {
+            numberOfHealthyKidneys=numberOfHealthyKidneys+1;
+        }
+    }
+    const numberOfUnhealthyKidneys=numberOfKidneys-numberOfHealthyKidneys;
+
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
+})
+
+app.listen(3000);
+
+
+///////FOR POST
+
+app.post("/",function(req,res){
+
+    const isHealthy=req.body.isHealthy;
+    user[0].kidneys.push({
+        healthy:isHealthy
+    })
+
+    res.json({
+        msg:"Done!"
+    })
+})
