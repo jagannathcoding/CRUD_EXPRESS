d3e748f4d591f876b85b45d527f587c0d2570c11
Jagannath <jagannathdebgunin@gmail.com>
2024-11-07 18:29:27 +0530

    Full code get post put delete

--- index.js
+++ index.js
@@ -76,4 +76,61 @@ app.put("/", function (req, res) {
     res.json({ success: true });
 });
 
+/*delete unhealthy kidney at all
+app.delete("/", function(req, res) {
+    if(isThereAtleastOneUnhealthyKidney()) {
+        const newKidneys = [];
+        for (let i = 0; i<user[0].kidneys.length; i++) {
+            if (user[0].kidneys[i].healthy) {
+                newKidneys.push({
+                    healthy: true
+                })
+            }
+        }
+        user[0].kidneys = newKidneys;
+        res.json({msg: "done"})   
+    } else {
+        res.status(411).json({
+            msg: "You have no bad kidneys"
+        });
+    }
+})
 
+function isThereAtleastOneUnhealthyKidney() {
+    let atleastOneUnhealthyKidney = false;
+    for (let i = 0; i<user[0].kidneys.length; i++) {
+        if (!user[0].kidneys[i].healthy) {
+            atleastOneUnhealthyKidney = true;
+        }
+    }
+    return atleastOneUnhealthyKidney
+}
+*/
+
+
+/*delete unhealthy kidney one by one*/
+
+
+app.delete("/", function(req, res) {
+    // Find the index of the first unhealthy kidney
+    const index = findFirstUnhealthyKidney();
+    
+    // If an unhealthy kidney exists, remove it
+    if (index !== -1) {
+        user[0].kidneys.splice(index, 1); // Remove the unhealthy kidney at the found index
+        res.json({ msg: "Removed one unhealthy kidney" });
+    } else {
+        res.status(411).json({
+            msg: "You have no bad kidneys"
+        });
+    }
+});
+
+function findFirstUnhealthyKidney() {
+    for (let i = 0; i < user[0].kidneys.length; i++) {
+        if (!user[0].kidneys[i].healthy) {
+            return i; // Return index of the first unhealthy kidney
+        }
+    }
+    return -1; // Return -1 if no unhealthy kidney is found
+}
